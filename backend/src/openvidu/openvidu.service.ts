d9f272a2ec08f83812f0facbd11345764e43bbc8
GEUMDOHYEON <dohyeon0518@gmail.com>
2024-05-02 00:49:36 +0900

    feat: create  openviduAPi

--- backend/src/openvidu/openvidu.service.ts
+++ backend/src/openvidu/openvidu.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { OpenVidu } from 'openvidu-node-client';
+import { OpenVidu,OpenViduRole } from 'openvidu-node-client';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -14,16 +14,18 @@ export class OpenviduService {
         this.openvidu = new OpenVidu(this.OPENVIDU_URL, this.OPENVIDU_SECRET);
     }
 
-    async createSessions(body:any){
+    async openviduTotalService(body:any){
         try {
-            const session = this.openvidu.activeSessions.find((s) => s.sessionId === body.customSessionId);
-            if(session !== undefined){
-                return body.customSessionId;
-            }else{
-                return await this.openvidu.createSession(body);
+            console.log(body);
+            const session = this.openvidu.activeSessions.find((s) => s.sessionId === body.userData.challengeId); // 동일한 세션이 있는지 검사
+            if(session !== undefined){ // 동일한 세션이 존재 O
+                // const connection = await session.
+                return this.createConnection(session.sessionId, body);
+            }else{ // 동일한 세션이 존재 X
+                const s = await this.openvidu.createSession({customSessionId: body.userData.challengeId});
+                return this.createConnection(s.sessionId, body);
+                // return connection.token;
             }
-            // const session = await this.openvidu.createSession(body);
-            // return session.sessionId;
         } catch (error) {
             console.log(error);
         }
@@ -31,25 +33,72 @@ export class OpenviduService {
 
     async createConnection(sessionId:string, body:any){
         const session = this.openvidu.activeSessions.find(
-            (s) => s.sessionId === sessionId,
+            (s) => s.sessionId === sessionId
         );
-
-        this.listActiveSessions();
+        
         if(!session){
-            throw new HttpException('Session not found',HttpStatus.NOT_FOUND);
+            throw new HttpException('Session not found', HttpStatus.NOT_FOUND);
         }else{
-            try{
-                const connection = await session.createConnection(body);
-                return connection.token;
-            }catch(error){
+            try {
+                const tokenOptions = {
+                    data: `{"userId": "${body.userData.userId}", "userName": "${body.userData.userName}"}`,
+                    role: OpenViduRole.PUBLISHER
+                };
+                const response = await session.generateToken(tokenOptions);
+                const token = this.extractToken(response);
+                return token;
+                
+            } catch (error) {
                 throw new HttpException(
-                    'Error creating connection: ' + error.message,
-                    HttpStatus.INTERNAL_SERVER_ERROR,
-                  );
-          
+                    'Error creating connection : ' + error.message,
+                    HttpStatus.INTERNAL_SERVER_ERROR 
+                )
             }
         }
     }
+
+
+    extractToken(url){
+        const queryParmas = new URLSearchParams(new URL(url).search);
+        const token = queryParmas.get('token');
+        return token;
+    }
+
+    // async createSessions(body:any){
+    //     try {
+    //         const session = this.openvidu.activeSessions.find((s) => s.sessionId === body.customSessionId);
+    //         if(session !== undefined){
+    //             return body.customSessionId;
+    //         }else{
+    //             const sessionId = await this.openvidu.createSession(body);
+    //             return sessionId.sessionId;
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // async createConnection(sessionId:string, body:any){
+    //     const session = this.openvidu.activeSessions.find(
+    //         (s) => s.sessionId === sessionId,
+    //     );
+
+    //     this.listActiveSessions();
+    //     if(!session){
+    //         throw new HttpException('Session not found',HttpStatus.NOT_FOUND);
+    //     }else{
+    //         try{
+    //             const connection = await session.createConnection(body);
+    //             return connection.token;
+    //         }catch(error){
+    //             throw new HttpException(
+    //                 'Error creating connection: ' + error.message,
+    //                 HttpStatus.INTERNAL_SERVER_ERROR,
+    //               );
+          
+    //         }
+    //     }
+    // }
 
     async listActiveSessions() {
         try {
