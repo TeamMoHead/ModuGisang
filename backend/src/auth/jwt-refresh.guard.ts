4820ef73b4eca2ac147c2b51bfc4f775ebe3308a
GEUMDOHYEON <dohyeon0518@gmail.com>
2024-05-03 22:46:59 +0900

    fix: jwt auth(api/auth, api/login, api/logout)

--- backend/src/auth/jwt-refresh.guard.ts
+++ backend/src/auth/jwt-refresh.guard.ts
@@ -5,6 +5,14 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class JwtRefreshGuard extends AuthGuard('jwt-refresh-token'){
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        return super.canActivate(context);
+        const request = context.switchToHttp().getRequest();
+        const refreshToken = request.body.refresh_token;
+        const authorizationHeader = request.headers.authorization;
+        const token = authorizationHeader.split(' ')[1];
+        if (token === "null"){
+            return true;
+        }else{
+            return super.canActivate(context);
+        }
     }
 }
