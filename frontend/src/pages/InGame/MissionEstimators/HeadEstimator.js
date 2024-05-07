4819fa71878382f383f284667a9043f72843e388
ifindary <ifindary@gmail.com>
2024-05-08 00:33:25 +0900

    feat: Mission 2 logic

--- frontend/src/pages/InGame/MissionEstimators/HeadEstimator.js
+++ frontend/src/pages/InGame/MissionEstimators/HeadEstimator.js
@@ -3,12 +3,13 @@ import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 
 let currentStatus; // 현재 고개를 돌린 방향
 let myMissionStatus = false; // 측정 결과
-const rotateDirections = ['상', '하', '좌', '우']; // 회전 방향 배열
+// const rotateDirections = ['상', '하', '좌', '우']; // 회전 방향 배열
 const numberDirections = 8; // 선택할 방향 수
 let selectedDirection = ['좌', '우', '좌', '상', '하', '우', '하', '상']; // 시도해야 할 방향 목록
-const timeoutDuration = 10000; // 총 제한 시간
+const timeoutDuration = 15000; // 총 제한 시간
 let isTimeOut = false; // 타임 아웃 여부
 let isEstimated = false; // 측정 완료 여부
+let isCentered = false;
 
 let currentSuccessCount = 0; // 성공 횟수 카운트
 let isGameStart = false; // 게임 시작 여부
@@ -34,16 +35,15 @@ export const estimateHead = ({ results, myVideoRef, canvasRef }) => {
     const leftMouth = poseLandmarks[pose.POSE_LANDMARKS.LEFT_RIGHT];
     const rightMouth = poseLandmarks[pose.POSE_LANDMARKS.RIGHT_LEFT];
 
-    console.log('------ pose.POSE_LANDMARKS:', pose.POSE_LANDMARKS);
-    console.log('------ leftMouth:', leftMouth);
-    console.log('------ rightMouth:', rightMouth);
-
     const centerSholderY = (leftShoulder.y + rightShoulder.y) / 2;
     const centerMouthY = (leftMouth.y + rightMouth.y) / 2;
 
     // 머리가 일정 범위 내에서 움직이는지 확인
-    const rangeShoulderX = Math.abs(leftShoulder.x - rightShoulder.x) * 0.3; // 어깨 간 거리의 30%로 범위 설정
-    const rangeShoulderY = Math.abs(leftShoulder.y - nose.y) * 1.5; // 어깨와 코의 y 좌표 차이에 상수를 곱하여 범위 설정
+    const rangeShoulderX = Math.abs(leftShoulder.x - rightShoulder.x) * 0.1; // 어깨 간 거리의 30%로 범위 설정
+
+    const distanceShoulderY = Math.abs(centerSholderY - nose.y); // 어깨와 코의 y 좌표 차이
+    const distanceLeftHeadX = Math.abs(leftEar.x - nose.x);
+    const distanceRightHeadX = Math.abs(rightEar.x - nose.x);
 
     // 정해진 시간이 지나면 타임아웃
     const handleTimeout = () => {
@@ -53,8 +53,11 @@ export const estimateHead = ({ results, myVideoRef, canvasRef }) => {
 
     // 1. 얼굴을 상, 하, 좌, 우 중 어느 쪽으로 돌려야 할지 미리 정한다.
     // 어느 방향으로 돌릴지 선택
-    if (selectedDirection.length === 0 && !isGameStart) {
-      isGameStart = true; // 여러 번 안 넣도록 임시 안전 장치... 동시성 문제 없으려나?
+    if (
+      // selectedDirection.length === 0 &&
+      !isGameStart
+    ) {
+      isGameStart = true; // 여러 번 안 넣도록 임시 안전 장치
 
       // ============= 특정 순서만 출력하도록 변경되어 주석 처리
       // 8번 반복하여 방향을 랜덤하게 선택하여 리스트에 추가
@@ -63,67 +66,95 @@ export const estimateHead = ({ results, myVideoRef, canvasRef }) => {
       //   selectedDirection.push(rotateDirections[randomDirectionIndex]); // 랜덤한 방향을 리스트에 추가
       // }
 
-      console.log('------- selectedDirection: ', selectedDirection);
-
       // 10초 타이머 설정
       setTimeout(handleTimeout, timeoutDuration);
+      console.log('------ 카운트 다운 시작');
     }
 
     // 2. N초 동안 얼굴을 해당 방향으로 돌렸는지 확인한다.
-
+    // 한 번 고개를 돌린 뒤에는 정면을 봐야 점수를 줌
     if (!isTimeOut) {
       // 머리가 일정 범위 내에서 움직이는지 확인
+      // 머리가 상하로 돌아갔는지 확인
       if (
-        nose.x > leftShoulder.x - rangeShoulderX &&
-        nose.x < rightShoulder.x + rangeShoulderX &&
-        nose.y > leftShoulder.y - rangeShoulderY &&
-        nose.y < leftShoulder.y + rangeShoulderY
+        nose.x < leftEar.x &&
+        nose.x > rightEar.x &&
+        centerSholderY - centerMouthY > (centerMouthY - nose.y) * 4.5 &&
+        isCentered
       ) {
-        currentStatus = '고개를 정면으로 향했습니다.';
-      } else {
-        // 머리가 상하로 돌아갔는지 확인
-        if (centerSholderY - centerMouthY < (centerMouthY - nose.y) * 3) {
-          currentStatus = '고개를 상으로 돌렸습니다.';
-        } else if (nose.y > leftShoulder.y) {
-          currentStatus = '고개를 하로 돌렸습니다.';
-        }
+        currentStatus = '상';
+        isCentered = false;
+        console.log('------------ ', currentStatus);
+      } else if (
+        nose.x < leftEar.x &&
+        nose.x > rightEar.x &&
+        centerSholderY - centerMouthY < (centerMouthY - nose.y) * 1.7 &&
+        isCentered
+      ) {
+        currentStatus = '하';
+        isCentered = false;
+        console.log('------------ ', currentStatus);
+      } else if (
+        nose.x > leftEar.x &&
+        nose.x > rightEar.x &&
+        Math.abs(leftEar.x - nose.x) >= Math.abs(rightEar.x - leftEar.x) &&
+        isCentered
+      ) {
+        currentStatus = '우';
+        isCentered = false;
+        console.log('------------ ', currentStatus);
+      } else if (
+        nose.x < leftEar.x &&
+        nose.x < rightEar.x &&
+        // Math.abs(rightEar.x - nose.x) <= Math.abs(rightEar.x - leftEar.x) &&
+        isCentered
+      ) {
+        currentStatus = '좌';
+        isCentered = false;
+        console.log('------------ ', currentStatus);
+      } else if (
+        nose.x < leftEar.x &&
+        nose.x > rightEar.x &&
+        centerSholderY - centerMouthY < (centerMouthY - nose.y) * 4 &&
+        centerSholderY - centerMouthY > (centerMouthY - nose.y) * 2
+        // && nose.y > leftShoulder.y - rangeShoulderY &&
+        // nose.y < leftShoulder.y + rangeShoulderY
+      ) {
+        currentStatus = '정면';
+        isCentered = true;
+        // console.log('------------ ', currentStatus);
+        // console.log('------------ isCentered: ', isCentered);
+      }
 
-        // 머리가 좌우로 돌아갔는지 확인
-        // 조건 수정 필요!!!!!!! --------
-        if (nose.x < (leftShoulder.x + rightShoulder.x) / 2.4) {
-          currentStatus = '좌';
-          console.log('------------ ', currentStatus);
-        } else if (nose.x > (leftShoulder.x + rightShoulder.x) / 1.8) {
-          currentStatus = '우';
-          console.log('------------ ', currentStatus);
-          // 상, 하 조건도 추가해야 함
-        } else {
-          currentStatus = '정면';
-        }
+      // 3. 같은 방향으로 돌렸다면 점수를 준다.
+      if (currentStatus === selectedDirection[0]) {
+        // 현재 상태가 다음 방향과 일치하는지 확인
+        selectedDirection.shift(); // 다음 방향으로 이동
 
-        // 3. 같은 방향으로 돌렸다면 점수를 준다.
-        if (currentStatus === selectedDirection[0]) {
-          // 현재 상태가 다음 방향과 일치하는지 확인
-          selectedDirection.shift(); // 다음 방향으로 이동
-
-          currentSuccessCount += 1;
-          console.log('------------ CURRENT SCORE: ', currentSuccessCount);
-        }
+        currentSuccessCount += 1;
+        console.log('------------ CURRENT SCORE: ', currentSuccessCount);
       }
     }
 
     // 4. 1~3을 3번 반복해서 점수가 일정 이상 쌓였다면 성공으로 판별한다.
-    if (!myMissionStatus && isTimeOut && !isEstimated) {
-      if (currentSuccessCount >= numberDirections) {
-        // 방향을 모두 수행했을 경우
-        myMissionStatus = true; // 성공
-        console.log('------------ FINAL RESULT: 성공!!!!!!!');
-        console.log('---------- Final Score:', currentSuccessCount);
-      } else {
-        myMissionStatus = false; // 성공
-        console.log('------------ FINAL RESULT: 실패.......');
-        console.log('---------- Final Score:', currentSuccessCount);
-      }
+    if (
+      !myMissionStatus &&
+      !isTimeOut &&
+      !isEstimated &&
+      currentSuccessCount >= numberDirections
+    ) {
+      // 방향을 모두 수행했을 경우
+      myMissionStatus = true; // 성공
+      console.log('------------ FINAL RESULT: 성공!!!!!!!');
+      console.log('---------- Final Score:', currentSuccessCount);
+    } else if (
+      isTimeOut &&
+      !isEstimated &&
+      currentSuccessCount < numberDirections
+    ) {
+      myMissionStatus = false; // 실패
+      console.log('------------ FINAL RESULT: 실패.......');
+      console.log('---------- Final Score:', currentSuccessCount);
 
       // 한 번만 출력 결과를 출력하도록
       isEstimated = true;
