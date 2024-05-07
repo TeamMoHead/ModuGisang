aeb958a88727cb0c22b240251ad5ec0c7a6bf46d
GEUMDOHYEON <dohyeon0518@gmail.com>
2024-05-08 00:04:00 +0900

    feat : mission3 arrow

--- frontend/src/pages/InGame/Mission3/Mission3.js
+++ frontend/src/pages/InGame/Mission3/Mission3.js
@@ -2,8 +2,33 @@ import React, { useRef, useEffect, useContext, useState } from 'react';
 import { GameContext, OpenViduContext } from '../../../contexts';
 import { Pose } from '@mediapipe/pose';
 import { estimateHead } from '../MissionEstimators/HeadEstimator';
+import bottomArrow from '../../../assets/arrows/bottom.png';
+import topArrow from '../../../assets/arrows/top.png';
+import leftArrow from '../../../assets/arrows/left.png';
+import rightArrow from '../../../assets/arrows/right.png';
 
 import styled from 'styled-components';
+
+const arrowImages = {
+  top: topArrow,
+  bottom: bottomArrow,
+  left: leftArrow,
+  right: rightArrow,
+};
+
+const round1 = [
+  { direction: 'top', active: false },
+  { direction: 'bottom', active: false },
+  { direction: 'left', active: false },
+  { direction: 'right', active: false },
+];
+
+const round2 = [
+  { direction: 'bottom', active: false },
+  { direction: 'right', active: false },
+  { direction: 'left', active: false },
+  { direction: 'right', active: false },
+];
 
 const Mission3 = () => {
   const {
@@ -16,6 +41,11 @@ const Mission3 = () => {
   const { myVideoRef } = useContext(OpenViduContext);
   const canvasRef = useRef(null);
   const msPoseRef = useRef(null);
+
+  // 화살표 세팅
+  const [arrowRound, setArrowRound] = useState([round1, round2]);
+  const [currentRoundIdx, setCurrentRoundIdx] = useState(0);
+  const [currentArrowIdx, setCurrentArrowIdx] = useState(0);
 
   useEffect(() => {
     if (inGameMode !== 3 || !myVideoRef.current) return;
@@ -38,9 +68,36 @@ const Mission3 = () => {
     });
 
     msPoseRef.current.onResults(results => {
-      setMyMissionStatus(estimateHead({ results, myVideoRef, canvasRef }));
+      let direction = arrowRound[currentRoundIdx][currentArrowIdx].direction;
+      const result = estimateHead({
+        results,
+        myVideoRef,
+        canvasRef,
+        direction,
+      });
+      handleDirection(result);
+      setMyMissionStatus(result);
       if (isGameLoading) setIsGameLoading(false);
     });
+
+    const handleDirection = result => {
+      // 해당 동작이 성공했다고 가정
+      if (result) {
+        let roundIdx = currentRoundIdx;
+        let arrowIdx = currentArrowIdx;
+        arrowRound[roundIdx][arrowIdx].active = true;
+
+        if (currentRoundIdx === 1 && currentArrowIdx === 3) {
+          //게임 끝
+          console.log('게임 끝');
+        } else if (currentRoundIdx === 0 && currentArrowIdx === 3) {
+          setCurrentRoundIdx(roundIdx++);
+          setCurrentArrowIdx(0);
+        } else {
+          setCurrentArrowIdx(arrowIdx++);
+        }
+      }
+    };
 
     const handleCanPlay = () => {
       let frameCount = 0;
@@ -69,7 +126,21 @@ const Mission3 = () => {
     };
   }, []);
 
-  return <Canvas ref={canvasRef} />;
+  return (
+    <>
+      <Canvas ref={canvasRef} />
+      <ArrowBox>
+        {arrowRound[currentRoundIdx].map(({ direction, active }) => (
+          <Arrows src={arrowImages[direction]} alt={active} />
+        ))}
+
+        {/* <Arrows src={leftArrow} alt="bottomArrow" />
+        <Arrows src={bottomArrow} alt="bottomArrow" />
+        <Arrows src={topArrow} alt="bottomArrow" />
+        <Arrows src={bottomArrow} alt="bottomArrow" /> */}
+      </ArrowBox>
+    </>
+  );
 };
 
 export default Mission3;
@@ -82,4 +153,18 @@ const Canvas = styled.canvas`
   width: 100vw;
   height: 100vh;
   object-fit: cover;
+`;
+
+const ArrowBox = styled.div`
+  position: fixed;
+  top: 150px;
+  width: 100%;
+  height: 100px;
+  ${({ theme }) => theme.flex.between}
+  background-color: ${({ theme }) => theme.colors.lighter.dark};
+`;
+// 위치 배열 조절하고 판정하는 코드로부터 값 받아오면 색상 변경
+const Arrows = styled.img`
+  width: 80px;
+  height: 50px;
 `;
