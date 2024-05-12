368618865feb71ca8c37e2953c4ca8519ee37b67
won0845 <tpqms775@gmail.com>
2024-05-12 22:56:30 +0900

    feat : 받아오는 타입 유효성 검사 전역적으로 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = configService.get<number>('NEST_PORT');
   app.enableCors({
     origin: '*',
