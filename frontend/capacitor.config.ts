869ea414eaff740d8dbfd21ae47d55278509a467
pongpongie <arevolvera@gmail.com>
2024-07-27 01:23:21 +0900

    feat: convert into iOS

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.modugisang.site',
+  appName: '모두기상',
+  webDir: 'build'
+};
+
+export default config;
